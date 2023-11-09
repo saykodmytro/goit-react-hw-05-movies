d0e1041a10aeb5dd55f2291a8be1b2f97fbefac8
Dmytro Sayko <saykodmytro@icloud.com>
2023-11-09 22:48:45 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <header className="main-header">
+        <NavLink className="nav-link" to="/">
+          Home
+        </NavLink>
+        <NavLink className="nav-link" to="/movies">
+          Movies
+        </NavLink>
+      </header>
+    </div>
+  );
 };
 
 export default App;
