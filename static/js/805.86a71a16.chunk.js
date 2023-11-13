0776500335bcf8cab44e00ca93e58128b09ea148
saykodmytro <64913341+saykodmytro@users.noreply.github.com>
2023-11-13 08:57:33 +0000

    Deploying to gh-pages from @ saykodmytro/goit-react-hw-05-movies@918dd7f77451cc098ac1073eb79c06c6b2d686ce 🚀

---
+++ static/js/805.86a71a16.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[805],{805:function(e,t,s){s.r(t);s(791);var o=s(87),a=s(184);t.default=function(){return(0,a.jsxs)("div",{className:"not-found",children:["Sorry, this page does not exist. Please follow this link to the",(0,a.jsx)(o.rU,{to:"/",children:(0,a.jsx)("span",{className:"text-home",children:" home page"})})]})}}}]);
+//# sourceMappingURL=805.86a71a16.chunk.js.map
